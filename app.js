cc2d075f4682a134555752db3c8729610f0c3a01
@shrutika3007! <shrutikajaware@gmail.com>
2025-08-14 14:55:10 +0530

    port change

--- app.js
+++ app.js
@@ -108,6 +108,7 @@ app.use((err, req, res, next) => {
     // res.status(statusCode).send(message);
 });
 
-app.listen(8080, () => {
-    console.log("server is listening to port 8080");
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
